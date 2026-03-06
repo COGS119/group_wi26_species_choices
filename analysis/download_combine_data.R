48e1696886f7eff0273e92b07e9bf2618403f0b5
mzettersten <mzettersten@users.noreply.github.com>
2026-03-05 18:42:08 -0800

    first push species choices

---
+++ analysis/download_combine_data.R
@@ -0,0 +1,67 @@
+library(here)
+library(tidyverse)
+
+#process data
+source(here("osf.r"))
+
+#specify OSF path
+osf_path <-  "f5pj7"
+file_name <- "species_choices"
+data_directory <- here("..","data")
+raw_data_directory <- here("..","data","raw_data")
+processed_data_directory <- here("..","data","processed_data")
+
+#download data
+#create data folder if it does not yet exist
+if (!dir.exists(data_directory)) {
+  dir.create(data_directory)
+} else {
+  print("Data Directory already exists!")
+}
+
+#as long as there isn't already an existing folder
+#download the raw data
+if (!dir.exists(raw_data_directory)) {
+  get_raw_data(osf_path)
+} else {
+  print("Raw Data Directory already exists!")
+}
+
+#as long as there isn't already an existing folder
+#create a processed data folder
+if (!dir.exists(processed_data_directory)) {
+  dir.create(processed_data_directory) 
+} else {
+  print("Processed Data Directory already exists!")
+}
+
+#merge data
+merge_data <- function(raw_data_directory,merged_data_path) {
+  
+  #collect raw data files
+  raw_files <- here(raw_data_directory, dir(here(raw_data_directory), "*.csv"))
+  
+  #read data
+  raw_data <- map_df(raw_files, function(file) {
+    d <- read_csv(file) 
+    
+    #check if colum participant exists, if not create it with NA values
+    if (!"participant" %in% colnames(d)) {
+      d <- d %>% mutate(participant = NA)
+    }
+    
+    d <- d %>% 
+      mutate(
+        file_name = file 
+      ) %>%
+      mutate(participant=as.character(participant))
+  }) 
+  
+  #keep cleaned data for verifying participation
+  write_csv(raw_data, here(merged_data_path,paste0(file_name,"-alldata.csv")))
+  
+  return(raw_data)
+}
+
+#execute the merge
+merged_data <- merge_data(raw_data_directory, processed_data_directory)
